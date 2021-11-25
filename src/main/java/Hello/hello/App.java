a45a82cd092c6169d81455d1b717442fb3318239
sheeba ashraf <sheeba.ashraf@gmail.com>
2021-11-25 20:04:55 +0400

    first commit

---
+++ src/main/java/Hello/hello/App.java
@@ -0,0 +1,14 @@
+package Hello.hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("hu");
+    }
+}
